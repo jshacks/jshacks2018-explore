55ecab0092edddd3d096bab9da107c0e1a812517
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2018-10-28 14:02:27 +0200

    Updated the view for Events.

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -4,13 +4,20 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 
+function random1Or2() {
+    return +((Math.random() * 10).toFixed(0)) % 2 + 1;
+}
+
 class Event extends Component {
     render() {
-
         return (
             <Card className='ex-event'>
                 <CardContent>
                     <h3>{this.props.event.name}</h3>
+                    <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+                        <small>Location: London</small>
+                        <small>Date: 12/0{random1Or2()}/2018</small>
+                    </div>
                 </CardContent>
                 <CardActions>
                     <div className={`ex-auth-buttons`}>
